4f194bd343524fa2a508a3acdf0b0089c5969e42
Vitor Levi <levivitor8@gmail.com>
2025-10-29 16:21:16 -0300

    feat(controller): Create register controller

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,5 @@
 import fastify from "fastify"
-import z from "zod"
-import { prisma } from "./lib/prisma.js"
+import { appRoutes } from "./http/routes/route.js"
 export const app = fastify()
 
-app.post('/users', async (request, reply) => {
-  const registerBodySchema = z.object({
-    name: z.string(),
-    email: z.email(),
-    password: z.string().min(6)
-  })
-  const { name, email, password } = registerBodySchema.parse(request.body)
-
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash: password
-    }
-  })
-})
+app.register(appRoutes)
