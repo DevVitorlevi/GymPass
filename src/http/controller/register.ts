3f132f6116e3d9d0fefc2c78b64fa29d91f5b9c6
Vitor Levi <levivitor8@gmail.com>
2025-12-11 17:03:28 -0300

    feat(controller): Create hash password

--- src/http/controller/register.ts
+++ src/http/controller/register.ts
@@ -1,5 +1,6 @@
 import { prisma } from "@src/lib/prisma.js";
 import type { FastifyReply, FastifyRequest } from "fastify";
+import { hash } from "bcryptjs";
 import z from "zod";
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
@@ -12,11 +13,21 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = resgisterBodySchema.parse(request.body)
 
+  const password_hash = await hash(password, 6)
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email
+    }
+  })
+  if (userWithSameEmail) {
+    return reply.status(409).send()
+  }
   await prisma.user.create({
     data: {
       name,
       email,
-      password_hash: password
+      password_hash
     }
   })
 
