012771f74dc9aaa6faa10e3bef0aaf2caa215fed
Vitor Levi <levivitor8@gmail.com>
2025-12-13 18:09:56 -0300

    feat(route): add route for login

--- src/presentation/routes/route.ts
+++ src/presentation/routes/route.ts
@@ -1,6 +1,8 @@
 import type { FastifyInstance } from "fastify";
-import { register } from "../controller/register.js";
+import { register } from "../controller/auth/register.js";
+import { login } from "../controller/auth/login.js";
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
+  app.post('/login', login)
 }
