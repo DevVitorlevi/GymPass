160cdee3bd1248a0d6b29bab5b7a8ba2e89efbe0
Vitor Levi <levivitor8@gmail.com>
2025-12-15 17:55:20 -0300

    feat(controller): add get user profile controller

---
+++ src/presentation/controller/user/getUserProfile.ts
@@ -0,0 +1,31 @@
+
+import { InvalidCredentialsError } from "@src/use-cases/erros/invalid-credentials.error.js";
+import { makeGetUserUseCase } from "@src/use-cases/factories/make-get-user-profile.use.js";
+import type { FastifyReply, FastifyRequest } from "fastify";
+export interface GetUserParams {
+  id: string
+}
+export async function getUserProfile(
+  request: FastifyRequest<{ Params: GetUserParams }>,
+  reply: FastifyReply
+) {
+  const { id } = request.params
+
+  try {
+    const getUserUseCase = makeGetUserUseCase()
+
+    const { user } = await getUserUseCase.execute({
+      userID: id,
+    })
+
+    return reply.status(200).send(user)
+
+  } catch (error) {
+    if (error instanceof InvalidCredentialsError) {
+      return reply.status(400).send({
+        message: error.message,
+      })
+    }
+    throw error
+  }
+}
