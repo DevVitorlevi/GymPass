2d4540239e677871cb0ad58cf66471fb1c8772b9
Vitor Levi <levivitor8@gmail.com>
2025-12-15 14:47:15 -0300

    refactor(user): use factory pattern for get user profile use case instantiation

---
+++ src/use-cases/factories/make-get-user-profile.use.ts
@@ -0,0 +1,9 @@
+import { PrismaUsersRepository } from "@src/repositories/prisma/prisma-users-repository.js";
+import { GetUserProfileUseCase } from "../auth/get-user-proflie.use-case.js";
+
+export function makeGetUserUseCase() {
+  const userRepository = new PrismaUsersRepository()
+  const getUserUseCase = new GetUserProfileUseCase(userRepository)
+
+  return getUserUseCase
+}
