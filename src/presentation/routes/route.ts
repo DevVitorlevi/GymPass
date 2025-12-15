bd0782a166bdb7ecb7f5ca73bfdafff0584ede49
Vitor Levi <levivitor8@gmail.com>
2025-12-15 18:00:28 -0300

    feat(route): add route for get user profile

--- src/presentation/routes/route.ts
+++ src/presentation/routes/route.ts
@@ -1,8 +1,10 @@
 import type { FastifyInstance } from "fastify";
-import { register } from "../controller/auth/register.js";
 import { login } from "../controller/auth/login.js";
+import { register } from "../controller/auth/register.js";
+import { getUserProfile } from "../controller/user/getUserProfile.js";
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/login', login)
+  app.get('/user/:id', getUserProfile)
 }
