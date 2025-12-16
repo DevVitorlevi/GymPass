6237c0f70b15a13a090a69597aac922999e75c5e
Vitor Levi <levivitor8@gmail.com>
2025-12-15 22:31:17 -0300

    feat(repository): implement findCheckInByUserIdOnDate method

--- src/repositories/in-memory/in-memory-check-in-repository.ts
+++ src/repositories/in-memory/in-memory-check-in-repository.ts
@@ -1,21 +1,36 @@
+import dayjs from "dayjs";
 import type { CheckIn, Prisma } from "generated/prisma/index.js";
 import { randomUUID } from "node:crypto";
 import type { CheckInsRepository } from "../check-ins-repository.interface.js";
 
 export class InMemoryCheckInsRepository implements CheckInsRepository {
-  public database: CheckIn[] = []
-  async create(data: Prisma.CheckInUncheckedCreateInput) {
+  public database: CheckIn[] = [];
 
+  async create(data: Prisma.CheckInUncheckedCreateInput) {
     const checkIn = {
       id: randomUUID(),
       validaded_at: data.validaded_at ? new Date(data.validaded_at) : null,
       gym_id: data.gym_id,
       user_id: data.user_id,
       create_at: new Date()
+    };
+
+    this.database.push(checkIn);
+
+    return checkIn;
+  }
+
+  async findCheckInByUserIdOnDate(user_id: string, date: Date) {
+    const checkIn = this.database.find(checkIn => {
+      const isSameDay = dayjs(checkIn.create_at).isSame(date, 'date');
+
+      return checkIn.user_id === user_id && isSameDay;
+    });
+
+    if (!checkIn) {
+      return null;
     }
 
-    this.database.push(checkIn)
-
-    return checkIn
+    return checkIn;
   }
 }
