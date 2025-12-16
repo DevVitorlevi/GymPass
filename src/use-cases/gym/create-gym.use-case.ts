651493d6bd1e7ffb92eaa899d8d88a98d2f3f765
Vitor Levi <levivitor8@gmail.com>
2025-12-16 13:44:03 -0300

    feat(use case): create use case for create gym

---
+++ src/use-cases/gym/create-gym.use-case.ts
@@ -0,0 +1,27 @@
+import type { GymsRepository } from "@src/repositories/gyms-repository.interface.js"
+import type { Gym } from "generated/prisma/index.js"
+
+interface GymUseCaseRequest {
+  title: string,
+  description: string | null
+  phone: string | null
+  latitude: number,
+  longitude: number
+}
+
+interface GymUseCaseResponse {
+  gym: Gym
+}
+
+export class GymUseCase {
+  constructor(private gymsRepository: GymsRepository) { }
+
+  async execute({ title, description, phone, latitude, longitude }: GymUseCaseRequest): Promise<GymUseCaseResponse> {
+    const gym = await this.gymsRepository.create({
+      title, description, phone, latitude, longitude
+    })
+
+    return { gym }
+  }
+
+}
