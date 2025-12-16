e9e0644822d4d6f02518680212f3c08d25f82b08
Vitor Levi <levivitor8@gmail.com>
2025-12-15 22:32:05 -0300

    test: create unit test for should not be able to check in twice in same day and should be able to check in twice in different days

--- src/use-cases/checkIn/check-in-user-case.spec.ts
+++ src/use-cases/checkIn/check-in-user-case.spec.ts
@@ -1,22 +1,64 @@
 import { InMemoryCheckInsRepository } from "@src/repositories/in-memory/in-memory-check-in-repository.js";
-import { beforeEach, describe, expect, it } from "vitest";
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 import { CheckInUseCase } from "./check-in.use.case.js";
 
-let checkinsRepository: InMemoryCheckInsRepository
-let sut: CheckInUseCase
+let checkinsRepository: InMemoryCheckInsRepository;
+let sut: CheckInUseCase;
 
 describe("Check In Use Case", () => {
   beforeEach(() => {
-    checkinsRepository = new InMemoryCheckInsRepository()
-    sut = new CheckInUseCase(checkinsRepository)
-  })
+    checkinsRepository = new InMemoryCheckInsRepository();
+    sut = new CheckInUseCase(checkinsRepository);
+
+    // Mock da data para um valor fixo
+    vi.useFakeTimers();
+  });
+
+  afterEach(() => {
+    // Restaura os timers reais após cada teste
+    vi.useRealTimers();
+  });
 
   it("should be able to create check in", async () => {
     const { checkIn } = await sut.execute({
       gymID: "gym-id",
       userID: "user-id"
-    })
+    });
 
-    expect(checkIn.id).toEqual(expect.any(String))
-  })
-})
+    expect(checkIn.id).toEqual(expect.any(String));
+  });
+
+  it("should not be able to check in twice in same day", async () => {
+    vi.setSystemTime(new Date(2024, 0, 20, 8, 0, 0));
+
+    await sut.execute({
+      gymID: 'gym-1',
+      userID: 'user-1'
+    });
+
+    await expect(
+      sut.execute({
+        gymID: 'gym-1',
+        userID: 'user-1'
+      })
+    ).rejects.toBeInstanceOf(Error);
+  });
+
+  it("should be able to check in twice in different days", async () => {
+    vi.setSystemTime(new Date(2024, 0, 20, 8, 0, 0));
+
+    await sut.execute({
+      gymID: 'gym-1',
+      userID: 'user-1'
+    });
+
+    vi.setSystemTime(new Date(2024, 0, 21, 8, 0, 0));
+
+    const { checkIn } = await sut.execute({
+      gymID: 'gym-1',
+      userID: 'user-1'
+    });
+
+    expect(checkIn.id).toEqual(expect.any(String));
+  });
+});
