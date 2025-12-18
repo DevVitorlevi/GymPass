f1f38256c74e34200276b8831bb7ce967b8a4e53
Vitor Levi <levivitor8@gmail.com>
2025-12-18 14:59:09 -0300

    feat(repository): implement findById method in PrismaCheckInsRepository

--- src/repositories/prisma/prisma-check-ins-repository.ts
+++ src/repositories/prisma/prisma-check-ins-repository.ts
@@ -3,10 +3,31 @@ import type { CheckIn, Prisma } from "generated/prisma/index.js";
 import type { CheckInsRepository } from "../check-ins-repository.interface.js";
 
 export class PrismaCheckInsRepository implements CheckInsRepository {
+  findCheckInByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null> {
+    throw new Error("Method not implemented.");
+  }
+  findManyCheckInsByUserId(userId: string, page: number): Promise<CheckIn[]> {
+    throw new Error("Method not implemented.");
+  }
+  findByUserMetrics(userId: string): Promise<number> {
+    throw new Error("Method not implemented.");
+  }
+  save(checkIn: CheckIn): Promise<CheckIn> {
+    throw new Error("Method not implemented.");
+  }
 
   async create(data: Prisma.CheckInUncheckedCreateInput) {
     const checkIn = await prisma.checkIn.create({
       data
+    })
+    return checkIn
+  }
+
+  async findById(checkInId: string) {
+    const checkIn = await prisma.checkIn.findUnique({
+      where: {
+        id: checkInId
+      }
     })
     return checkIn
   }
