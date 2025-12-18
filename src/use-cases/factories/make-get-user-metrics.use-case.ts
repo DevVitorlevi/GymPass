6244f6cbf508b4e30f7e9b8207e4fbe1fb06b822
Vitor Levi <levivitor8@gmail.com>
2025-12-18 16:15:49 -0300

    feat: create factory pattern for get user metrics use case instantiation

---
+++ src/use-cases/factories/make-get-user-metrics.use-case.ts
@@ -0,0 +1,8 @@
+import { PrismaCheckInsRepository } from "@src/repositories/prisma/prisma-check-ins-repository.js";
+import { GetUserMetricsUseCase } from "../users/get-user-metrics.use.case.js";
+export function makeGetUserMetricsUseCase() {
+  const checkInsRepository = new PrismaCheckInsRepository()
+  const getUserMetricsUseCase = new GetUserMetricsUseCase(checkInsRepository)
+
+  return getUserMetricsUseCase
+}
