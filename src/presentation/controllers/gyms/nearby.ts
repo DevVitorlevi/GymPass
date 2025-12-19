400c19842f09f17d3626d34007d7e9dafff00fa3
Vitor Levi <levivitor8@gmail.com>
2025-12-19 20:27:10 -0300

    feat(controller): add nearby gyms controller

---
+++ src/presentation/controllers/gyms/nearby.ts
@@ -0,0 +1,28 @@
+
+import { makeFetchGymsNearbyUseCase } from "@src/use-cases/factories/make-fetch-gyms-nearby.use-case.js";
+import type { FastifyReply, FastifyRequest } from "fastify";
+import z from "zod";
+
+export async function nearby(request: FastifyRequest, reply: FastifyReply) {
+
+  const nearbyGymsQueryParams = z.object({
+    latitude: z.number().refine(value => {
+      return Math.abs(value) <= 90
+    }),
+    longitude: z.number().refine(value => {
+      return Math.abs(value) <= 180
+    })
+  })
+
+
+  const { latitude, longitude } = nearbyGymsQueryParams.parse(request.body)
+
+
+  const gymUseCase = makeFetchGymsNearbyUseCase()
+  const { gyms } = await gymUseCase.execute({ userlatitude: latitude, userlongitude: longitude })
+
+  return reply.status(200).send({
+    gyms,
+  })
+
+}
